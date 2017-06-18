fc7e028f0386d98350274d9f193b388635a32078
Dung Le <phieudu241@yahoo.com>
2017-06-18 16:26:54 +0700

    Implement scraping items on list search

--- index.js
+++ index.js
@@ -7,22 +7,19 @@ var cheerio = require('cheerio');
 var _ = require('lodash');
 var CONSTANTS = require('./constants');
 var app = express();
-
-var ITEMs_URL = "http://bless.gamemeca.com/item/getItem.php";
-
-//var PROXY = '';
-var PROXY = 'http://192.168.78.7:8888';
+var PROXY = '';
+//var PROXY = 'http://192.168.78.7:8888';
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static('public'));
 
 app.get('/test', function (req, res) {
-    res.send('Hello');
+    res.send('Hello2');
 });
 
-app.get('/scrapeItemIds', function (req, res) {
-    scrapeItemIds(res);
+app.get('/scrapeItems', function (req, res) {
+    scrapeItemIds();
     res.send('Doing....');
 });
 
@@ -31,31 +28,32 @@ app.get('/convertToCsv', function (req, res) {
     res.send('Doing....');
 });
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
 });
 
-async function scrapeItemIds(res) {
-    // request.post({
-    //     url: ITEMs_URL,
-    //     proxy: PROXY,
-    //     form: {itemCategory: 20, p: 1}
-    // }, function (error, response, html) {
-    //     if (!error) {
-    //         res.send(JSON.parse(response.body));
-    //     } else {
-    //         res.sendStatus(500);
-    //     }
-    // });
-    var body = getItemIds(1);
-    console.log('hi');
-    console.log(body);
+async function scrapeItemIds() {
+    let allItems = [];
+    let items = [];
+    let numOfPages = 1;
+
+    for (var i = 1; i <= numOfPages; i++) {
+        items = await getItems(1, i);
+        allItems = allItems.concat(items.data);
+    }
+
+    fs.writeFileSync(CONSTANTS.ITEMS_JSON_FILE_PATH, JSON.stringify(allItems));
 }
 
-async function getItemIds(page) {
-    return await requestp.post({
-        url: ITEMs_URL,
-        proxy: PROXY,
-        form: {itemCategory: 20, p: page}
-    });
+async function getItems(itemCategory, page) {
+    try {
+        return await requestp.post({
+            url: ITEMs_URL,
+            proxy: PROXY,
+            form: {itemCategory: itemCategory, p: page},
+            json: true
+        });
+    } catch (err) {
+        return {data: []};
+    }
 };
